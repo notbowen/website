6e05849e79a4e8bf3522a366050c125c1da9218b
notbowen <contact@hubowen.dev>
2025-09-01 13:26:39 +0800

    feat: add KaTeX

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export default defineConfig({
   site: "https://hubowen.dev",
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  },
   integrations: [mdx(), sitemap(), tailwind(), icon()],
 });
