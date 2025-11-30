443ee430a29248b5401b63fd60df55d1aede283e
Hu Bowen <contact@hubowen.dev>
2025-11-30 12:29:58 +0800

    chore: add Cloudflare Pages integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,19 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
-import rehypeKatex from 'rehype-katex';
-import remarkMath from 'remark-math';
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://hubowen.dev",
+
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex]
+    rehypePlugins: [rehypeKatex],
   },
+
   integrations: [mdx(), sitemap(), tailwind(), icon()],
+  adapter: cloudflare(),
 });
